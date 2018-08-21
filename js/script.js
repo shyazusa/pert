4c85c74c41332e0f1b7b876dbb11ae45bc243ba1
shy_azusa <shy.azusa@gmail.com>
2018-08-21 10:07:01 +0900

    Add calcTime

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const calcTime = () => {
+  const best = Number(document.getElementById('best').value);
+  const most = Number(document.getElementById('most').value);
+  const worst = Number(document.getElementById('worst').value);
+  const ans = Number((best + most * 4 + worst) / 6);
+  document.getElementById('ans').innerHTML = ans;
+}
