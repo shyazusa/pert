e51f4a042b10d53caa52fe3f390b94096fa465ac
shy_azusa <shy.azusa@gmail.com>
2018-09-12 17:44:23 +0900

    Use npm

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {loader: "style-loader"},
+          {loader: "css-loader"}
+        ]
+      }
+    ]
+  }
+};
